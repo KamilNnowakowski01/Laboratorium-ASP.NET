4a43ec9c2df6adbdd9d27ef20259690cae850785
KamilNnowakowski0 <KamilNnowakowski0>
2023-11-11 15:44:50 +0100

    Dodaj pliki Lab3.

---
+++ Lab 3/Controllers/ComputerController.cs
@@ -0,0 +1,146 @@
+﻿using Lab_3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Lab_3.Controllers
+{
+    public class ComputerController : Controller
+    {
+       
+        public ComputerController()
+        {
+            // Dodawanie przykładowych komputerów do kolekcji przy inicjalizacji kontrolera
+            if (!_computers.Any())
+            {
+                CreateExampleComputers();
+            }
+        }
+
+        static Dictionary<int, Computer> _computers = new Dictionary<int, Computer>();
+
+        public void CreateExampleComputers ()
+        {
+            Computer komputer1 = new Computer
+            {
+                Id = 1,
+                Name = "Komputer-2022",
+                Processor = "Intel Core i5",
+                MemoryRAM = "8",
+                GraphicsCard = "NVIDIA GeForce GTX 1650",
+                Manufacturer = "HP",
+                ProductionDate = new DateTime(2022, 1, 15)
+            };
+            Computer komputer2 = new Computer
+            {
+                Id = 2,
+                Name = "Komputer AMD",
+                Processor = "AMD Ryzen 7",
+                MemoryRAM = "16",
+                GraphicsCard = "AMD Radeon RX 6700 XT",
+                Manufacturer = "Dell",
+                ProductionDate = new DateTime(2021, 8, 22)
+            };
+            Computer komputer3 = new Computer
+            {
+                Id = 3,
+                Name = "Laptop For Coding",
+                Processor = "Intel Core i7",
+                MemoryRAM = "32",
+                GraphicsCard = "NVIDIA GeForce RTX 3080",
+                Manufacturer = "Asus",
+                ProductionDate = new DateTime(2020, 5, 10)
+            };
+            _computers.Add(komputer1.Id, komputer1);
+            _computers.Add(komputer2.Id, komputer2);
+            _computers.Add(komputer3.Id, komputer3);
+        }
+
+        public IActionResult Index()
+        {
+            return View(_computers);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Form");
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] Computer model)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = _computers.Keys.Count != 0 ? _computers.Keys.Max() : 0;
+                model.Id = id + 1;
+                _computers.Add(model.Id, model);
+
+                return RedirectToAction("Index");
+            }
+            return View("Form");
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (_computers.Keys.Contains(id))
+            {
+                return View(_computers[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Computer model)
+        {
+            if (ModelState.IsValid)
+            {
+                _computers[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (_computers.Keys.Contains(id))
+            {
+                return View(_computers[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            if (_computers.Keys.Contains(id))
+            {
+                _computers.Remove(id);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (_computers.Keys.Contains(id))
+            {
+                return View(_computers[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Lab 3/Models/Computer.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_3.Models
+{
+    public class Computer
+    {
+
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę komputera!")]
+        [MinLength(3, ErrorMessage = "Pole musi mieć co najmniej 3 znaki.")]
+        [MaxLength(50, ErrorMessage = "Pole nie może mieć więcej niż 50 znaków.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać model procesora!")]
+        [MinLength(2, ErrorMessage = "Pole musi mieć co najmniej 2 znaki.")]
+        [MaxLength(50, ErrorMessage = "Pole nie może mieć więcej niż 50 znaków.")]
+        public string Processor { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać ilość pamięci RAM!")]
+        [Range(2, 512, ErrorMessage = "Pole musi mieć wartość między 2 a 512.")]
+        public string MemoryRAM { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać model karty graficzną!")]
+        [MinLength(2, ErrorMessage = "Pole musi mieć co najmniej 2 znaki.")]
+        [MaxLength(50, ErrorMessage = "Pole nie może mieć więcej niż 50 znaków.")]
+        public string GraphicsCard { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać producenta!")]
+        [MinLength(2, ErrorMessage = "Pole musi mieć co najmniej 2 znaki.")]
+        [MaxLength(50, ErrorMessage = "Pole nie może mieć więcej niż 50 znaków.")]
+        public string Manufacturer { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Proszę podać poprawną datę produkcji!")]
+        [Range(typeof(DateTime), "1946-02-14", "2025-01-01", ErrorMessage = "Data nie znajduje się w przedziale 1946-02-14 - 2025-01-01.")]
+        public DateTime ProductionDate { get; set; }
+
+        public bool IsValid()
+        {
+            bool isName = Name != null;
+            bool isProcessor = Processor != null;
+            bool isMemoryRAM = MemoryRAM != null;
+            bool isGraphicsCard = GraphicsCard != null;
+            bool isManufacturer = Manufacturer != null;
+            bool isProductionDate = ProductionDate != null;
+
+            bool IsValid = isName && isProcessor && isMemoryRAM && isGraphicsCard && isManufacturer && isProductionDate;
+
+            return IsValid;
+        }
+    }
+}
